63017afc2ce3833d49529687ad8b0b8e64938c3b
danbotwat <133717903+danbotwat@users.noreply.github.com>
2024-10-04 18:30:21 -0400

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -1,4 +1,3 @@
 //Welcome to week 1 of blueprint!
-//Hi my name is Jane and here is a fun fact about me!
-//Hi my name is John and I love blueprint! 
-//Hi my name is Arushi and my favorite song is Rush by Troye Sivan at the moment!
+//Hi my name is Danny 
+//My favorite song at the moment is Look Ahead by Future
