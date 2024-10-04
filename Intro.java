874f2d6cd71c905d7c92be89b21d2552ba472369
r.r.dhi <dhingrabusiness@gmail.com>
2024-10-04 18:16:20 -0400

    Update Intro.java

--- Intro.java
+++ Intro.java
@@ -2,3 +2,4 @@
 //Hi my name is Jane and here is a fun fact about me!
 //Hi my name is John and I love blueprint! 
 //Hi my name is Arushi and my favorite song is Rush by Troye Sivan at the moment!
+//hey my name is rishabh dhingra and i am a sophmore
